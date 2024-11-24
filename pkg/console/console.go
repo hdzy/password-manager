1e6bc63c22ce2781d43a57990abc4f910f476cbe
Smirnov Oleg <smirnov.oleg.1231@gmail.com>
2024-11-24 21:06:39 +0300

    msg

--- pkg/console/console.go
+++ pkg/console/console.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/nsf/termbox-go"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -23,17 +24,70 @@ func Init() {
 	}
 	defer termbox.Close()
 
-	menuItems := []string{"Пункт 1", "Пункт 2", "Пункт 3", "Выход"}
+	if startMenu() == -1 {
+		return
+	}
+
+	<-time.NewTimer(10 * time.Second).C
+}
+
+func startMenu() int {
+	menuItems := []string{"Управление парольными политиками", "Управление паролями"}
 	currentItem := 0
 
-	ClearTerminal()
-	pickedValue := initMenu(menuItems, currentItem)
+	switch pickedValue := initMenu(menuItems, currentItem); pickedValue {
+	case 0:
+		if policyMenu() == -1 {
+			return startMenu()
+		}
+	case 1:
+		if passwordMenu() == -1 {
+			return startMenu()
+		}
+	case -1:
+		return -1
+	}
 
 	ClearTerminal()
 	RefreshTerminal()
+
+	return 1
+}
+
+func policyMenu() int {
+	menuItems := []string{"Посмотреть все парольные политики", "Создать новую парольную политику"}
+	currentItem := 0
+
+	pickedValue := initMenu(menuItems, currentItem)
+
+	if pickedValue == -1 {
+		return -1
+	}
+
+	ClearTerminal()
+	RefreshTerminal()
+
 	fmt.Println("Выбранный пункт:", pickedValue+1)
 
-	<-time.NewTimer(10 * time.Second).C
+	return 1
+}
+
+func passwordMenu() int {
+	menuItems := []string{"Посмотреть все пароли", "Создать новый пароль"}
+	currentItem := 0
+
+	pickedValue := initMenu(menuItems, currentItem)
+
+	if pickedValue == -1 {
+		return -1
+	}
+
+	ClearTerminal()
+	RefreshTerminal()
+
+	fmt.Println("Выбранный пункт:", pickedValue+1)
+
+	return 1
 }
 
 func initMenu(menuItems []string, currentItem int) int {
@@ -42,7 +96,7 @@ func initMenu(menuItems []string, currentItem int) int {
 
 	render := func() {
 		for i, item := range menuItems {
-			y := i + 1
+			y := i * 2
 			if i == currentItem {
 				printItem(item, termbox.ColorBlack, termbox.ColorWhite, y)
 			} else {
@@ -78,6 +132,8 @@ func initMenu(menuItems []string, currentItem int) int {
 		}
 	}
 
+	ClearTerminal()
+	RefreshTerminal()
 	render()
 	if eventHandler() {
 		return currentItem
@@ -97,6 +153,7 @@ func RefreshTerminal() {
 // Функция для вывода строки целиком
 func printItem(text string, fg, bg termbox.Attribute, y int) {
 	// Отображаем всю строку целиком
+	text = strings.ReplaceAll(text, " ", "  ")
 	for x, ch := range text {
 		termbox.SetCell(x/2, y, ch, fg, bg)
 	}
